edb530032e5266124419b21d08c9391768debb72
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-10-04 11:11:47 -0400

    feat: wire up an LLM call

--- lib/georesearch/agents/analyzer.rb
+++ lib/georesearch/agents/analyzer.rb
@@ -1,8 +1,18 @@
+require "ruby_llm"
+
 module Georesearch
   module Agents
     class Analyzer
+      attr_reader :response
+
       def self.analyze
-        puts "Going to analyze"
+        pp new.response
+      end
+
+      def initialize
+        @chat = RubyLLM.chat(model: "claude-sonnet-4")
+        response = @chat.ask "What up dawg?"
+        @response = response.content
       end
     end
   end
