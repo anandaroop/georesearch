3ab2262cf2d7330230a511bf9bd547c4eea123f8
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-10-04 11:11:28 -0400

    refactor: move sandbox to faff subcommand
    this way it picks up env vars, just as it would for end user

--- lib/georesearch/cli.rb
+++ lib/georesearch/cli.rb
@@ -15,7 +15,24 @@ module Georesearch
         end
       end
 
+      class Base < Dry::CLI::Command
+        def call(*)
+          Georesearch.configure_llm
+        end
+      end
+
+      class Faff < Base
+        # desc "Sandbox for trying things out"
+
+        def call(*)
+          super
+          require_relative "agents/analyzer"
+          Georesearch::Agents::Analyzer.analyze
+        end
+      end
+
       register "version", Version, aliases: ["v", "-v", "--version"]
+      register "faff", Faff
     end
   end
 end
